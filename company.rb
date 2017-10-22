410e797cf968589aa6fe3ed76614dd96574ade73
Tinus Mørch Abell <tinusabell@gmail.com>
2017-10-23 00:50:15 +0200

    Add comments

--- company.rb
+++ company.rb
@@ -3,14 +3,18 @@ require 'sinatra'
 require 'sinatra/namespace'
 require 'json'
 
+# Serve static client
 get '/' do
-  send_file File.join(settings.public_folder, 'index.html')
+	send_file File.join(settings.public_folder, 'index.html')
 end
 
+# URI versioning is used in place of eg. content-negotiation.
 namespace '/v1' do
 
+	# Storage for the companies. Takes the place of a database or other persistent storage.
 	$companies = {}
 
+	# Data model for a company
 	class Company
 		attr_reader :cvr, :name, :address, :city, :country, :phone
 
@@ -23,6 +27,7 @@ namespace '/v1' do
 			@phone = phone
 		end
 
+		# Returns all the fields of the company in hash format.
 		def to_hash
 			{
 				:cvr => @cvr, 
@@ -34,6 +39,7 @@ namespace '/v1' do
 			}
 		end
 
+		# Returns the company in hash format - including only name and cvr for display and lookup purposes.
 		def to_hash_short
 			{
 				:cvr => @cvr, 
@@ -42,6 +48,7 @@ namespace '/v1' do
 		end
 	end
 
+	# Resource to get a list of the all stored companies
 	get '/company' do
 		output = []
 		$companies.each do |key, company|
@@ -50,6 +57,7 @@ namespace '/v1' do
 		output.to_json
 	end
 
+	# Resource to get the details of a company with the given cvr.
 	get '/company/:cvr' do
 		cvr = params['cvr']
 		company = $companies[cvr]
@@ -61,19 +69,22 @@ namespace '/v1' do
 		end	
 	end
 
+	# Resource to store a company using the included data.
 	post '/company' do
 		request.body.rewind
 		companyJson = JSON.parse(request.body.read)
 
-		if !checkRequests(companyJson)
+		if !isValidRequest(companyJson)
 			return
 		end
 		newCompany = Company.new(companyJson['cvr'], companyJson['name'], companyJson['address'], companyJson['city'], companyJson['country'], companyJson['phone'])
 		$companies[newCompany.cvr] = newCompany
 		status 204
+		body "Company sucessfully created"
 	end
 
-	def checkRequests(data)
+	# Checks if a request to store a company is valid and if not sets the error message enumerating the missing values.
+	def isValidRequest(data)
 		nonOptionalParameters = ['cvr', 'name', 'address', 'city', 'country']
 		missingKeys = []
 
--- public/app.js
+++ public/app.js
@@ -3,8 +3,11 @@ var restcompany = angular.module('restcompany', []);
 restcompany.controller('mainController', ['$scope', '$http', 
     function ($scope, $http) {
     
+    // Data model
     $scope.companies=[];
     $scope.companyDetails;
+
+    // Form data model
     $scope.name;
     $scope.cvr;
     $scope.address;
@@ -12,32 +15,37 @@ restcompany.controller('mainController', ['$scope', '$http',
     $scope.country;
     $scope.phone;
 
+    // Get the company details via http
     $scope.getCompanyDetails = function (cvr) {
         $http.get('/v1/company/'+cvr, {}).then($scope.companyDetailsSucceeded, $scope.companyDetailsFailed);
     }
 
+    // Handle getting company details - set the data in the model.
     $scope.companyDetailsSucceeded = function(response) {
         $scope.companyDetails=response.data;
     }
 
+    // Simply alert user when getting company details failed.
     $scope.companyDetailsFailed = function(response) {
-        alert("DETAILS FAIL");
+        alert("DETAILS FAIL: "+JSON.stringify(response));
     }
 
+    // Get the list companies via http
     $scope.getCompanyList = function () {
         $http.get('/v1/company').then($scope.companyListSuceeded, $scope.companyListFailed);
     }
 
+    // Handle getting list of companies - set the data in the model.
     $scope.companyListSuceeded = function(response) {
         $scope.companies=response.data;
     }
 
+    // Simply alert user when getting list of companies failed.
     $scope.companyListFailed = function(response) {
-        alert("LIST FAIL");
+        alert("LIST FAIL: "+JSON.stringify(response));
     }
 
-    
-
+    // Post the content of the form data model via http
     $scope.companyCreate = function() {
         data = {
             'name': $scope.name,
@@ -52,6 +60,7 @@ restcompany.controller('mainController', ['$scope', '$http',
         $http.post('/v1/company', data, {}).then($scope.companyCreateSuceeded, $scope.companyCreateFailed);
     }
 
+    // Handle a new company bering created, refreshing list of companies, retrieve details of new company and reset form data model.
     $scope.companyCreateSuceeded = function(response) {
         $scope.getCompanyList();
         $scope.getCompanyDetails($scope.cvr);
@@ -64,8 +73,9 @@ restcompany.controller('mainController', ['$scope', '$http',
         $scope.phone=null;
     }
 
+    // Simply alert user when creating company failed. 
     $scope.companyCreateFailed = function(response) {
-        alert("CREATE FAIL");
+        alert("CREATE FAIL: "+JSON.stringify(response));
     }
 
     $scope.getCompanyList();
